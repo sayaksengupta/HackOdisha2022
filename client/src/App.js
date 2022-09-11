96a6ce482337779d59b2d105f6f675abfc5aedfb
Sayak Sengupta <sayaksengupta@Sayaks-MacBook-Air.local>
2022-09-11 19:50:56 +0530

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,13 @@ import Articles from "./pages/Articles/Articles";
 import Chat from "./pages/Chat/Chat";
 import Sidebar from "./components/Sidebar/Sidebar";
 import SignIn from "./pages/SignIn/SignIn";
-import SignUp from "./pages/SignUp/SignUp";;
+import SignUp from "./pages/SignUp/SignUp";
+import { useState, useEffect } from "react";
+import Profile from "./pages/Profile/Profile";
+import Help from "./pages/Help/Help";
+import { TeamFinderCard } from "./components/TeamFinder/TeamFinderCard";
+import { FeedCard } from "./components/FeedCard/FeedCard";
+import { EditProfile } from "./pages/Profile/EditProfile";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -18,12 +24,21 @@ function App() {
 
   return isLoggedIn ? (
     <Router>
-      
+      <Sidebar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/hackathons" element={<Hackathons />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/messages" element={<Chat />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/tf" element={<TeamFinderCard />} />
+          <Route path="/fc" element={<FeedCard />} />
+          <Route path="/profile/edit" element={<EditProfile />} />
 
           <Route path="*" element={<div>404 Page not found :(</div>} />
         </Routes>
+      </Sidebar>
     </Router>
   ) : (
     <Router>
